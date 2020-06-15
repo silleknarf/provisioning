3b14ed3a5b246706e2d162ae6475da882e56ab53
silleknarf@gmail.com <silleknarf@gmail.com>
2020-06-15 23:07:12 +0100

    Change from weave net to flannel

--- service/kubernetes/scripts/master.sh
+++ service/kubernetes/scripts/master.sh
@@ -3,6 +3,7 @@ set -e
 
 kubeadm init --config /tmp/master-configuration.yml \
   --ignore-preflight-errors=Swap,NumCPU
+  --pod-network-cidr=${overlay_cidr}
 
 kubeadm token create ${token}
 
@@ -14,7 +15,8 @@ until nc -z localhost 6443; do
   sleep 5
 done
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 
 # See: https://kubernetes.io/docs/admin/authorization/rbac/
 kubectl create clusterrolebinding permissive-binding \
